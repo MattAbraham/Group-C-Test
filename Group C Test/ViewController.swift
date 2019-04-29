0a074e973a9d7f9b95395c421f61549da9e0a839
Matthew Abraham (s5116305) <s5116305@mac38572.mac.bournemouth.ac.uk>
2019-04-29 10:09:57 +0100

    Initial Commit

---
+++ Group C Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Group C Test
+//
+//  Created by Matthew Abraham (s5116305) on 29/04/2019.
+//  Copyright © 2019 Matthew Abraham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
