b7919a8ea9ee99c8a5bb06a8dbacd270eb3937fe
Matthew Abraham (s5116305) <s5116305@mac42948.mac.bournemouth.ac.uk>
2019-04-30 11:25:13 +0100

    Progress View for image upload

--- Group C Test/ViewController.swift
+++ Group C Test/ViewController.swift
@@ -4,6 +4,8 @@ import Firebase
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressView: UIProgressView!
+    
     @IBAction func uploadButton(_ sender: AnyObject) {
         let imagePicker = UIImagePickerController()
         imagePicker.sourceType = .photoLibrary
@@ -16,15 +18,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
-
+   
+//    let observer = uploadTask.observe(.progress) { snapshot in
+//
+//    }
     
     func upload(_ image: UIImage) {
         let storage = Storage.storage()
         let imagesRef = storage.reference(withPath: "image")
         let data = image.jpeg(.medium)!
-        imagesRef.putData(data, metadata: nil) { meta, error in
-            print("finished")
+        let uploadTask = imagesRef.putData(data, metadata: nil) { meta, error in
+            if (error != nil) {
+                print("Error \(error?.localizedDescription)")
+            } else {
+                print("Upload Complete \(data)")
+            }
         }
+        uploadTask.observe(.progress) { [weak self] (snapshot) in
+            guard let strongSelf = self else { return }
+            guard let progress = snapshot.progress else { return }
+            strongSelf.progressView.progress = Float(progress.fractionCompleted)
+        }
+            
     }
     
 
@@ -34,7 +49,6 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
-    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.originalImage] as! UIImage
         upload(image)
