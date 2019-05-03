5fc0560988809bf415a121206cfce1ec2e0ced2a
Matthew Abraham (s5116305) <s5116305@mac38571.mac.bournemouth.ac.uk>
2019-05-03 14:31:52 +0100

    More clean up

--- Group C Test/ViewController.swift
+++ Group C Test/ViewController.swift
@@ -8,7 +8,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var imagePreview: UIImageView!
     @IBOutlet weak var captionText: UITextField!
     var post = Post()
-    
     @IBAction func getPhotoButton(_ sender: AnyObject) {
         let imagePicker = UIImagePickerController()
         imagePicker.sourceType = .photoLibrary
@@ -16,24 +15,18 @@ class ViewController: UIViewController {
         imagePicker.delegate = self
         present(imagePicker, animated: true, completion: nil)
     }
-    
     @IBAction func uploadButton(_ sender: Any) {
         let image = UIImage(named: "demo")!
         save(post)
-  
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-
 //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 //        let image = UIImage(named: "demo")!
 //        save(post)
 //    }
-    
-    
     func save(_ post: Post) {
         upload(post.image) { storageMeta, error in
             let ref = Firestore.firestore().collection("posts").document()
@@ -55,13 +48,10 @@ class ViewController: UIViewController {
         let data = image.jpegData(compressionQuality: 0.7)!
         let meta = StorageMetadata()
         meta.contentType = "image/jpeg"
-
-        //3. Save image to storage first
         imageStorageRef.putData(data, metadata: meta) { storageMeta, error in
             completion(storageMeta, error)
         }
     }
-    
     func loadImages() {
     let ref = Firestore.firestore().collection("posts")
         ref.getDocuments { snapshot, error in
@@ -71,7 +61,6 @@ class ViewController: UIViewController {
     }
     }
 }
-
     
 
 
